bc84ba3724bb0dc5c0ec1f4c3f4abbf05d027d1a
Nashuim <pontesaguinaldo15@gmail.com>
2014-04-12 15:16:00 +0100

    Edge interface defined

--- common-module/src/main/java/pt/isel/ps1314v/g11/common/graph/Edge.java
+++ common-module/src/main/java/pt/isel/ps1314v/g11/common/graph/Edge.java
@@ -1,5 +1,26 @@
 package pt.isel.ps1314v.g11.common.graph;
 
-public class Edge {
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
 
+/**
+ * A single edge with a target and value
+ *
+ *
+ * @param <I> Vertex Id
+ * @param <E> Edge value
+ */
+public interface Edge<I extends WritableComparable, E extends Writable>{
+
+	/**
+	 * 
+	 * @return Id of the edge's target Vertex
+	 */
+	I getTargetVertexId();
+	
+	/**
+	 * 
+	 * @return The edge value
+	 */
+	E getValue();
 }
