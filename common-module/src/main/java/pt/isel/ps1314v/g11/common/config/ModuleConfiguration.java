f0f6eae2a5019d8109d13a5f21bdfaeb49890d21
Nashuim <pontesaguinaldo15@gmail.com>
2014-04-14 20:27:20 +0100

    Code cleanup

--- common-module/src/main/java/pt/isel/ps1314v/g11/common/config/ModuleConfiguration.java
+++ common-module/src/main/java/pt/isel/ps1314v/g11/common/config/ModuleConfiguration.java
@@ -1,9 +1,6 @@
 package pt.isel.ps1314v.g11.common.config;
 
-import pt.isel.ps1314v.g11.common.graph.Aggregator;
-import pt.isel.ps1314v.g11.common.graph.Combiner;
 
-@SuppressWarnings("rawtypes")
 public interface ModuleConfiguration {
 	void useAlgorithm();
 	void setClass(String name,Class<?> value, Class<?> xface);
