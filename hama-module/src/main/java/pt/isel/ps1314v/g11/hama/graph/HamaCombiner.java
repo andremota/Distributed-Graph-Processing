19cb5803bd7752051156d421be3e38ef6239efae
Nashuim <pontesaguinaldo15@gmail.com>
2014-04-14 16:01:24 +0100

    HamaCombiner implemented

--- hama-module/src/main/java/pt/isel/ps1314v/g11/hama/graph/HamaCombiner.java
+++ hama-module/src/main/java/pt/isel/ps1314v/g11/hama/graph/HamaCombiner.java
@@ -1,5 +1,39 @@
 package pt.isel.ps1314v.g11.hama.graph;
 
-public class HamaCombiner {
+import org.apache.hadoop.conf.Configurable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.util.ReflectionUtils;
+
+import pt.isel.ps1314v.g11.common.graph.Combiner;
+
+public class HamaCombiner<M extends Writable> extends
+		org.apache.hama.bsp.Combiner<M> implements Configurable {
+
+	private Combiner<M> combiner;
+	private Configuration conf;
+
+	@Override
+	public M combine(Iterable<M> messages) {
+		M message = combiner.initialValue();
+
+		for (M m : messages) {
+			combiner.combine(message, m);
+		}
+		return message;
+	}
+
+	@Override
+	public Configuration getConf() {
+		return conf;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void setConf(Configuration conf) {
+		this.conf = conf;
+		combiner = (Combiner<M>) ReflectionUtils.newInstance(
+				conf.getClass(Combiner.COMBINER_CLASS, Combiner.class), conf);
+	}
 
 }
