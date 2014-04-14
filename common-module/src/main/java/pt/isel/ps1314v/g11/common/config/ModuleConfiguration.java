6a08e839707cd475ae626af6c14cff9930636a89
Nashuim <pontesaguinaldo15@gmail.com>
2014-04-14 19:45:10 +0100

    Started GiraphConfig implementation.

--- common-module/src/main/java/pt/isel/ps1314v/g11/common/config/ModuleConfiguration.java
+++ common-module/src/main/java/pt/isel/ps1314v/g11/common/config/ModuleConfiguration.java
@@ -6,8 +6,6 @@ import pt.isel.ps1314v.g11.common.graph.Combiner;
 @SuppressWarnings("rawtypes")
 public interface ModuleConfiguration {
 	void useAlgorithm();
-	void setAggregatorClass(Class<? extends Aggregator> aggregatorClass);
-	void setCombinerClass(Class<? extends Combiner> combinerClass);
 	void setClass(String name,Class<?> value, Class<?> xface);
 	void useAggregator();
 	void useCombiner();
--- giraph-module/src/main/java/pt/isel/ps1314v/g11/giraph/config/GiraphConfig.java
+++ giraph-module/src/main/java/pt/isel/ps1314v/g11/giraph/config/GiraphConfig.java
@@ -5,27 +5,62 @@ import org.apache.giraph.conf.GiraphConfiguration;
 import pt.isel.ps1314v.g11.common.config.ModuleConfiguration;
 import pt.isel.ps1314v.g11.common.graph.Aggregator;
 import pt.isel.ps1314v.g11.common.graph.Combiner;
+import pt.isel.ps1314v.g11.giraph.graph.AggregatorMasterCompute;
+import pt.isel.ps1314v.g11.giraph.graph.GiraphCombinerMapper;
+import pt.isel.ps1314v.g11.giraph.graph.GiraphComputationMapper;
 
 @SuppressWarnings("rawtypes")
-public class GiraphConfig extends GiraphConfiguration implements ModuleConfiguration{
+public class GiraphConfig implements ModuleConfiguration{
 
+	private GiraphConfiguration config;
+	
+	public public GiraphConfig(GiraphConfiguration config) {
+		this.config = config;
+	}
+	
 	@Override
 	public void useAlgorithm() {
-		// TODO Auto-generated method stub
+		config.setComputationClass(GiraphComputationMapper.class);
+		
+	}
+
+
+	@Override
+	public void useAggregator() {
+		config.setMasterComputeClass(AggregatorMasterCompute.class);
 		
 	}
 
 	@Override
-	public void setAggregatorClass(Class<? extends Aggregator> aggregatorClass) {
-		// TODO Auto-generated method stub
+	public void useCombiner() {
+		config.setMessageCombinerClass(GiraphCombinerMapper.class);
 		
 	}
 
 	@Override
-	public void setCombinerClass(Class<? extends Combiner> combinerClass) {
-		// TODO Auto-generated method stub
+	public void setClass(String name, Class<?> value, Class<?> xface) {
+		config.setClass(name,value,xface);
 		
 	}
+
+	@Override
+	public void setInt(String name, int value) {
+		config.setInt(name,value);
+		
+	}
+	
+	@Override
+	public void set(String name, String value) {
+		config.set(name,value);
+	}
+
+	@Override
+	public boolean run(boolean verbose) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
 
 
 }
