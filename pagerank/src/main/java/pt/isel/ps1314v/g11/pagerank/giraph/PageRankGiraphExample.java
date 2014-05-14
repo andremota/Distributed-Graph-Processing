4c931ff634ece11df3255506d149e84a13fcb720
André Mota <aqmota@gmail.com>
2014-05-14 20:19:59 +0100

    Added AdjacencyListWithValuesInputFormat to giraph-module.
    Changed PageRankGiraphExample to use AdjacencyListWithValuesInputFormat.

--- pagerank/src/main/java/pt/isel/ps1314v/g11/pagerank/giraph/PageRankGiraphExample.java
+++ pagerank/src/main/java/pt/isel/ps1314v/g11/pagerank/giraph/PageRankGiraphExample.java
@@ -1,13 +1,13 @@
 package pt.isel.ps1314v.g11.pagerank.giraph;
 
 import org.apache.giraph.conf.GiraphConfiguration;
+import org.apache.giraph.io.formats.IdWithValueTextOutputFormat;
 import org.apache.giraph.utils.InternalVertexRunner;
 
 import pt.isel.ps1314v.g11.common.config.CommonConfig;
 import pt.isel.ps1314v.g11.giraph.config.GiraphModuleConfiguration;
+import pt.isel.ps1314v.g11.giraph.io.AdjacencyListWithValuesInputFormat;
 import pt.isel.ps1314v.g11.heatkernel.RandomWalkAlgorithm;
-import pt.isel.ps1314v.g11.heatkernel.giraph.io.HeatKernelInputFormat;
-import pt.isel.ps1314v.g11.heatkernel.giraph.io.HeatKernelOutputFormat;
 import pt.isel.ps1314v.g11.pagerank.PageRankAlgorithm;
 
 public class PageRankGiraphExample {
@@ -20,10 +20,10 @@ public class PageRankGiraphExample {
 		 */
 		conf.set("giraph.SplitMasterWorker", "false");
 
-		conf.setVertexInputFormatClass(HeatKernelInputFormat.class);
-		conf.setVertexOutputFormatClass(HeatKernelOutputFormat.class);
+		conf.setVertexInputFormatClass(AdjacencyListWithValuesInputFormat.class);
+		conf.setVertexOutputFormatClass(IdWithValueTextOutputFormat.class);
 		conf.setWorkerConfiguration(1, 1, 100);
-		conf.setInt(RandomWalkAlgorithm.MAX_SUPERSTEPS_CONF, 30);
+		conf.setInt(RandomWalkAlgorithm.MAX_SUPERSTEPS_CONF, 1);
 		
 		GiraphModuleConfiguration giraphConfig = new GiraphModuleConfiguration(conf);
 		CommonConfig commonConfig = new CommonConfig(giraphConfig);
