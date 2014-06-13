1d6d4808209d57c0c6a885a0637593e35b51554f
Aguinaldo Pontes <pontesaguinaldo15@gmail.com>
2014-06-13 17:59:54 +0100

    Parser in BC

---
+++ Betweenness-Centrality/src/main/java/pt/isel/ps1314v/g11/bc/util/Config.java
@@ -0,0 +1,39 @@
+package pt.isel.ps1314v.g11.bc.util;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.spi.BooleanOptionHandler;
+import org.kohsuke.args4j.spi.StringArrayOptionHandler;
+
+public class Config {
+
+	@Option(name="-s", usage="Sets the start vertices for Betweenness Centrality, space separated list of integers."
+			+ " Default is all vertices"
+			, metaVar = "startsList", handler=StringArrayOptionHandler.class)
+	private String[] bcStart;
+	
+	@Option(name="-n", usage="Use to see a normalized output for Betweenness Centrality"
+			, handler=BooleanOptionHandler.class)
+	private boolean bcNormal;
+	
+	@Argument(usage="Sets the input file.", metaVar = "in")
+	private String inFile;
+	
+	@Argument(usage="Sets the output file.", metaVar = "out")
+	private String outFile;
+	
+	public static Config parseArgs(String[] args) throws CmdLineException{
+		Config config = new Config();
+		CmdLineParser parser = new CmdLineParser(config);
+		try {
+			parser.parseArgument(args);
+		} catch (CmdLineException e) {
+			parser.printUsage(System.out);
+			throw e;
+		}
+		return config;
+	}
+	
+}
