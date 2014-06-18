b73c46bbb645b7f863c4265e1f09515a252d8a0e
André Mota <aqmota@gmail.com>
2014-06-18 18:38:43 +0100

    Changed ModuleJobRunner and GiraphModuleJobRunner.

--- common-module/src/main/java/pt/isel/ps1314v/g11/common/config/ModuleJobRunner.java
+++ common-module/src/main/java/pt/isel/ps1314v/g11/common/config/ModuleJobRunner.java
@@ -2,7 +2,11 @@ package pt.isel.ps1314v.g11.common.config;
 
 import java.io.IOException;
 
+import org.kohsuke.args4j.CmdLineException;
+
 public interface ModuleJobRunner {
-	public abstract boolean run(String[] args) throws IOException, ClassNotFoundException, InterruptedException;
+	public abstract boolean run(String[] args) throws IOException,
+			ClassNotFoundException, InterruptedException, CmdLineException;
+
 	public abstract JobBean createJobBean();
 }
--- giraph-module/src/main/java/pt/isel/ps1314v/g11/giraph/config/GiraphModuleJobRunner.java
+++ giraph-module/src/main/java/pt/isel/ps1314v/g11/giraph/config/GiraphModuleJobRunner.java
@@ -3,9 +3,9 @@ package pt.isel.ps1314v.g11.giraph.config;
 import java.io.IOException;
 
 import org.apache.giraph.conf.GiraphConfiguration;
+import org.apache.giraph.io.formats.GiraphFileInputFormat;
 import org.apache.giraph.job.GiraphJob;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
@@ -18,7 +18,7 @@ public abstract class GiraphModuleJobRunner implements ModuleJobRunner {
 
 	@Override
 	public boolean run(String[] args) throws IOException,
-			ClassNotFoundException, InterruptedException {
+			ClassNotFoundException, InterruptedException, CmdLineException {
 
 		GiraphConfiguration conf = new GiraphConfiguration();
 		GiraphModuleConfiguration giraphConfig = new GiraphModuleConfiguration(
@@ -30,15 +30,11 @@ public abstract class GiraphModuleJobRunner implements ModuleJobRunner {
 
 		JobBean bean = createJobBean();
 		CmdLineParser parser = new CmdLineParser(bean);
-		try {
-			parser.parseArgument(args);
-		} catch (CmdLineException e) {
-			throw new IOException(e);
-		}
+		parser.parseArgument(args);
 		
 		prepareJob(job, conf, commonConfig, bean);
 		
-		FileInputFormat.setInputPath(job.getInternalJob(), new Path(bean.getInputPath()));
+		GiraphFileInputFormat.addVertexInputPath(conf, new Path(bean.getInputPath()));
 		FileOutputFormat.setOutputPath(job.getInternalJob(), new Path(bean.getOutputPath()));
 
 		return job.run(bean.verbose());
