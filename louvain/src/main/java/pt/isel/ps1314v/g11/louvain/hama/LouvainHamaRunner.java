b7355a54622daee04c869cdd4e24a1d6ced997af
Aguinaldo Pontes <pontesaguinaldo15@gmail.com>
2014-06-18 18:52:08 +0100

    LouvainJobRunner done

---
+++ louvain/src/main/java/pt/isel/ps1314v/g11/louvain/hama/LouvainHamaRunner.java
@@ -0,0 +1,44 @@
+package pt.isel.ps1314v.g11.louvain.hama;
+
+import org.apache.hama.bsp.TextInputFormat;
+import org.apache.hama.graph.GraphJob;
+
+import pt.isel.ps1314v.g11.common.aggregator.BooleanOrAggregator;
+import pt.isel.ps1314v.g11.common.aggregator.LongSumAggregator;
+import pt.isel.ps1314v.g11.common.config.CommonConfig;
+import pt.isel.ps1314v.g11.common.config.JobBean;
+import pt.isel.ps1314v.g11.hama.config.HamaModuleJobRunner;
+import pt.isel.ps1314v.g11.louvain.LouvainAlgorithm;
+import pt.isel.ps1314v.g11.louvain.hama.io.LouvainTextReader;
+
+public class LouvainHamaRunner extends HamaModuleJobRunner{
+
+	@Override
+	public JobBean createJobBean() {
+		return new JobBean();
+	}
+
+	@Override
+	public void prepareJob(GraphJob job, CommonConfig moduleConfig, JobBean bean) {
+		job.setJobName(LouvainAlgorithm.class.getSimpleName());
+	    // Vertex reader
+		job.setVertexInputReaderClass(LouvainTextReader.class);
+
+		job.setInputFormat(TextInputFormat.class);
+
+		
+		//job.setPartitioner(HashPartitioner.class);
+		
+
+		moduleConfig.setAlgorithmClass(LouvainAlgorithm.class);
+		moduleConfig.registerAggregator(LouvainAlgorithm.AGG_M2, LongSumAggregator.class);
+		moduleConfig.registerAggregator(LouvainAlgorithm.CHANGE_AGG, BooleanOrAggregator.class);
+
+		
+		moduleConfig.preparePlatformConfig();
+	    
+		
+		
+	}
+
+}
--- louvain/src/main/java/pt/isel/ps1314v/g11/louvain/hama/LouvainInHamaExample.java
+++ louvain/src/main/java/pt/isel/ps1314v/g11/louvain/hama/LouvainInHamaExample.java
@@ -4,57 +4,22 @@ import java.io.IOException;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hama.HamaConfiguration;
 import org.apache.hama.bsp.HashPartitioner;
 import org.apache.hama.bsp.TextInputFormat;
 import org.apache.hama.bsp.TextOutputFormat;
-import org.apache.hama.graph.Edge;
 import org.apache.hama.graph.GraphJob;
-import org.apache.hama.graph.Vertex;
-import org.apache.hama.graph.VertexInputReader;
 
 import pt.isel.ps1314v.g11.common.aggregator.BooleanOrAggregator;
 import pt.isel.ps1314v.g11.common.aggregator.LongSumAggregator;
 import pt.isel.ps1314v.g11.common.config.CommonConfig;
 import pt.isel.ps1314v.g11.hama.config.HamaModuleConfiguration;
 import pt.isel.ps1314v.g11.louvain.LouvainAlgorithm;
-import pt.isel.ps1314v.g11.louvain.LouvainVertexValue;
+import pt.isel.ps1314v.g11.louvain.hama.io.LouvainTextReader;
 
 public class LouvainInHamaExample {
 
-	public static class LouvainTextReader extends VertexInputReader<LongWritable, Text,
-	LongWritable, IntWritable, LouvainVertexValue>{
-		//private Logger LOG = Logger.getLogger(KCoreDecompositionInHamaExample.class);
-		@Override
-		public boolean parseVertex(
-				LongWritable key,
-				Text value,
-				Vertex<LongWritable, IntWritable, LouvainVertexValue> vertex)
-				throws Exception {
-			
-			String[] ws = value.toString().split(" ");
-
-			vertex.setVertexID(new LongWritable(Long.parseLong(ws[0])));
-
-			for (int i = 2; i < ws.length; i += 2) {
-				vertex.addEdge(new Edge<LongWritable, IntWritable>(
-						new LongWritable(Long.parseLong(ws[i])),
-						new IntWritable(Integer.parseInt(ws[i + 1]))));
-			}
-/*
-			vertex.setVertexID(new LongWritable(Long.parseLong(key.toString())));
-			for(Writable w: value.get()){
-				vertex.addEdge(new Edge<LongWritable, IntWritable>(
-						new LongWritable(Long.parseLong(w.toString())),new IntWritable(1)));
-			}
-*/
-			return true;
-		}
-		
-	}
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException{
 			
 			HamaConfiguration conf =  new HamaConfiguration();
