5e3da387c93788bba38a2b0651caf4f209141cee
André Mota <aqmota@gmail.com>
2014-06-18 18:18:04 +0100

    Done GiraphModuleJobRunner.

--- giraph-module/src/main/java/pt/isel/ps1314v/g11/giraph/config/GiraphModuleJobRunner.java
+++ giraph-module/src/main/java/pt/isel/ps1314v/g11/giraph/config/GiraphModuleJobRunner.java
@@ -11,28 +11,35 @@ import pt.isel.ps1314v.g11.common.config.CommonConfig;
 import pt.isel.ps1314v.g11.common.config.JobBean;
 import pt.isel.ps1314v.g11.common.config.ModuleJobRunner;
 
-public abstract class GiraphModuleJobRunner implements ModuleJobRunner{
+public abstract class GiraphModuleJobRunner implements ModuleJobRunner {
 
 	@Override
-	public boolean run(String[] args) throws IOException, ClassNotFoundException, InterruptedException, CmdLineException {
-		
+	public boolean run(String[] args) throws IOException,
+			ClassNotFoundException, InterruptedException {
+
 		GiraphConfiguration conf = new GiraphConfiguration();
-		GiraphModuleConfiguration giraphConfig = new GiraphModuleConfiguration(conf);
+		GiraphModuleConfiguration giraphConfig = new GiraphModuleConfiguration(
+				conf);
 		CommonConfig commonConfig = new CommonConfig(giraphConfig);
-		
-		GiraphJob job = new GiraphJob(conf, commonConfig.getAlglorithmClass().getSimpleName());
-		
+
+		GiraphJob job = new GiraphJob(conf, commonConfig.getAlglorithmClass()
+				.getSimpleName());
+
 		JobBean bean = createJobBean();
 		CmdLineParser parser = new CmdLineParser(bean);
-		parser.parseArgument(args);
-		
-		prepareJob(job,conf,commonConfig,bean);
-		
-		return job.run(bean.verbose);
+		try {
+			parser.parseArgument(args);
+		} catch (CmdLineException e) {
+			throw new IOException(e);
+		}
+
+		prepareJob(job, conf, commonConfig, bean);
+
+		return job.run(bean.verbose());
 	}
 
-	
-	public abstract void  prepareJob(GiraphJob job, GiraphConfiguration conf, CommonConfig commonConfig, JobBean bean);
-	
+	public abstract void prepareJob(GiraphJob job, GiraphConfiguration conf,
+			CommonConfig commonConfig, JobBean bean);
+
 	public abstract JobBean createJobBean();
 }
