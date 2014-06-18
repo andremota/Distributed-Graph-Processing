c8e731fef537b53fddb6b3ffe5c0f224dee2c5fc
Aguinaldo Pontes <pontesaguinaldo15@gmail.com>
2014-06-18 18:59:59 +0100

    KCoreHamaRunner

---
+++ k-core/src/main/java/pt/isel/ps1314v/g11/k_core/hama/KCoreHamaRunner.java
@@ -0,0 +1,29 @@
+package pt.isel.ps1314v.g11.k_core.hama;
+
+import org.apache.hama.bsp.TextInputFormat;
+import org.apache.hama.bsp.TextOutputFormat;
+import org.apache.hama.graph.GraphJob;
+
+import pt.isel.ps1314v.g11.common.config.CommonConfig;
+import pt.isel.ps1314v.g11.common.config.JobBean;
+import pt.isel.ps1314v.g11.hama.config.HamaModuleJobRunner;
+import pt.isel.ps1314v.g11.k_core.KCoreDecompositionAlgorithm;
+import pt.isel.ps1314v.g11.k_core.hama.KCoreDecompositionInHamaExample.KCoreTextReader;
+
+public class KCoreHamaRunner extends HamaModuleJobRunner{
+
+	@Override
+	public JobBean createJobBean() {
+		return new JobBean();
+	}
+
+	@Override
+	public void prepareJob(GraphJob job, CommonConfig moduleConfig, JobBean bean) {
+		job.setVertexInputReaderClass(KCoreTextReader.class);
+		job.setInputFormat(TextInputFormat.class);
+		job.setOutputFormat(TextOutputFormat.class);
+		
+		moduleConfig.setAlgorithmClass(KCoreDecompositionAlgorithm.class);
+	}
+
+}
