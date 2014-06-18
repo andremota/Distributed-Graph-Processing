1125c13fccdc6b775218a11dbeb4111381876a2b
André Mota <aqmota@gmail.com>
2014-06-18 18:31:54 +0100

    Changed ModuleJobRunner interface.

--- common-module/src/main/java/pt/isel/ps1314v/g11/common/config/JobBean.java
+++ common-module/src/main/java/pt/isel/ps1314v/g11/common/config/JobBean.java
@@ -5,7 +5,7 @@ import org.kohsuke.args4j.spi.BooleanOptionHandler;
 
 public abstract class JobBean {
 	@Option(name = "-v", usage = "Uses verbose or not.", metaVar = "in", required = false, handler = BooleanOptionHandler.class)
-	public boolean verbose = true;
+	private boolean verbose = true;
 
 	@Option(name = "-i", usage = "Sets the input file.", metaVar = "in", required = true)
 	private String inFile;
--- common-module/src/main/java/pt/isel/ps1314v/g11/common/config/ModuleJobRunner.java
+++ common-module/src/main/java/pt/isel/ps1314v/g11/common/config/ModuleJobRunner.java
@@ -4,5 +4,5 @@ import java.io.IOException;
 
 public interface ModuleJobRunner {
 	public abstract boolean run(String[] args) throws IOException, ClassNotFoundException, InterruptedException;
-	public abstract JobBean createJobBean(String[] args);
+	public abstract JobBean createJobBean();
 }
