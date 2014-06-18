68a3a67a1987e7746e96d4aec02cd395b269db53
André Mota <aqmota@gmail.com>
2014-06-18 18:04:20 +0100

    Changed GiraphModuleJobRunner.

--- common-module/src/main/java/pt/isel/ps1314v/g11/common/config/ModuleJobRunner.java
+++ common-module/src/main/java/pt/isel/ps1314v/g11/common/config/ModuleJobRunner.java
@@ -1,5 +1,7 @@
 package pt.isel.ps1314v.g11.common.config;
 
+import java.io.IOException;
+
 public interface ModuleJobRunner {
-	public abstract boolean run(String[] args);
+	public abstract boolean run(String[] args) throws IOException, ClassNotFoundException, InterruptedException;
 }
--- giraph-module/src/main/java/pt/isel/ps1314v/g11/giraph/config/GiraphModuleJobRunner.java
+++ giraph-module/src/main/java/pt/isel/ps1314v/g11/giraph/config/GiraphModuleJobRunner.java
@@ -1,5 +1,7 @@
 package pt.isel.ps1314v.g11.giraph.config;
 
+import java.io.IOException;
+
 import org.apache.giraph.conf.GiraphConfiguration;
 import org.apache.giraph.job.GiraphJob;
 
@@ -9,18 +11,18 @@ import pt.isel.ps1314v.g11.common.config.ModuleJobRunner;
 public abstract class GiraphModuleJobRunner implements ModuleJobRunner{
 
 	@Override
-	public boolean run(String[] args) {
+	public boolean run(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		
 		GiraphConfiguration conf = new GiraphConfiguration();
 		GiraphModuleConfiguration giraphConfig = new GiraphModuleConfiguration(conf);
 		CommonConfig commonConfig = new CommonConfig(giraphConfig);
-		prepareConfig(conf,commonConfig);
 		
-		//GiraphJob job = new GiraphJob(conf.get, giraphConfig);
+		GiraphJob job = new GiraphJob(conf, commonConfig.getAlglorithmClass().getSimpleName());
+		prepareJob(job,conf,commonConfig);
 		
-		return false;
+		return job.run(Boolean.parseBoolean(args[0]));
 	}
 
 	
-	public abstract void  prepareConfig(GiraphConfiguration conf, CommonConfig commonConfig);
+	public abstract void  prepareJob(GiraphJob job, GiraphConfiguration conf, CommonConfig commonConfig);
 }
